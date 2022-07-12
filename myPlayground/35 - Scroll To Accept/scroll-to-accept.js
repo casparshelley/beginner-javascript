2baa92ec0da23dc75beec53f0899df2a6428d4e7
Caspar Shelley <cshelley@cshelle-ltmbz3p.internal.salesforce.com>
2022-07-12 16:51:12 +0100

    Ex 35 done

--- myPlayground/35 - Scroll To Accept/scroll-to-accept.js
+++ myPlayground/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,14 +1,35 @@
+/* eslint-disable no-console */
 function scrollToAccept() {
   // good practice to use a function that breaks if selector is not present
+
   const terms = document.querySelector('.terms-and-conditions');
+  const watch = document.querySelector('.watch');
+  const button = document.querySelector('.accept');
 
   if (!terms) {
     return;
   }
 
+  function obCallBack(payload) {
+    // console.log(payload[0].intersectionRatio);
+    if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+
+      // stop observing the button
+      ob.unobserve(terms.lastElementChild);
+    }
+  }
+
+  const ob = new IntersectionObserver(obCallBack, {
+    root: terms,
+    threshold: 1,
+  });
+
+  ob.observe(terms.lastElementChild);
+
   terms.addEventListener('scroll', (e) => {
-    console.log(e.currentTarget.scrollTop);
-    console.log(e.currentTarget.scrollHeight);
+    // console.log(e.currentTarget.scrollTop);
+    // console.log(e.currentTarget.scrollHeight);
   });
 }
 
